8bcd80083c5ae511c4f3d43a638a3a45f4f9d78a
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-05-16 17:38:28 -0700

    save

--- src/components/OrderCard/components/BdayModal/BdayModal.jsx
+++ src/components/OrderCard/components/BdayModal/BdayModal.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Confetti from 'react-confetti'
 import "./BdayModal.css"
 
 const BdayModal = ({showBday}) => {
+  const [showConfetti, setShowConfetti] = useState(false)
+
+
+  useEffect(()=>{
+      if(showBday){
+        setTimeout(()=>{
+        setShowConfetti(true)
+        },2500);
+      }
+
+  },[showBday])
   return (
     <div className={showBday ? "bday-modal show-bday" : "bday-modal"}>
    
         <div className="family-img-div">
-        <div className="bday-header">
+         <div className={showBday ? "bday-header animate-header" : "bday-header"}>
         <h1>Happy</h1>
         <h1>Birthday!!</h1>
-      </div>
-          <img className="family-img" src="colinwhitney.jpeg" alt="" />
+      </div> 
+          <img className={showBday ? "family-img animate-img" : "family-img"} src="colinwhitney.jpeg" alt="" />
+          {/* <img className="family-img" src="colinwhitney.jpeg" alt="fuckyou" /> */}
         </div>
-        <Confetti/>
+        {showConfetti && <Confetti/>}
     </div>
   )
 }
